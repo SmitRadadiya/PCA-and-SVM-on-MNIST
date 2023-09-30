60a80d8092ea678c7d7e9d3d04f28d0a1766a509
Smit Radaddiya <radadiya.smit03@gmail.com>
2023-09-30 16:29:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from typing import Tuple
 
 def get_hyperparameters() -> Tuple[float, int, float]:
     # get the hyperparameters
+    learning_rate = 0.0001  #0.001 less acc(84%)
+    num_iters = 270000  # 240000
+    C = 10  #10
+    return learning_rate,num_iters,C
     raise NotImplementedError
 
 
@@ -19,27 +23,27 @@ def main() -> None:
     X_train, X_test = normalize(X_train, X_test)
 
     metrics = []
-    for k in [5, 10, 20, 50, 100, 200, 500]:
+    for k in [5, 10, 20, 50, 100, 200, 500]:    # 
         # reduce the dimensionality of the data
         pca = PCA(n_components=k)
-        X_train = pca.fit_transform(X_train)
-        X_test = pca.transform(X_test)
+        X_train_emb = pca.fit_transform(X_train)
+        X_test_emb = pca.transform(X_test)
 
         # create a model
         svm = MultiClassSVM(num_classes=10)
 
         # fit the model
         svm.fit(
-            X_train, y_train, C=C,
+            X_train_emb, y_train, C=C,
             learning_rate=learning_rate,
             num_iters=num_iters,
         )
 
         # evaluate the model
-        accuracy = svm.accuracy_score(X_test, y_test)
-        precision = svm.precision_score(X_test, y_test)
-        recall = svm.recall_score(X_test, y_test)
-        f1_score = svm.f1_score(X_test, y_test)
+        accuracy = svm.accuracy_score(X_test_emb, y_test)
+        precision = svm.precision_score(X_test_emb, y_test)
+        recall = svm.recall_score(X_test_emb, y_test)
+        f1_score = svm.f1_score(X_test_emb, y_test)
 
         metrics.append((k, accuracy, precision, recall, f1_score))
 
